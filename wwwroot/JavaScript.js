21f9ed5ebce0ad257f965a84769f2d1cfd47cf38
DonBeltul <150187311+DonBeltul@users.noreply.github.com>
2024-04-01 14:01:41 +0200

    Agregar archivos de proyecto.

---
+++ wwwroot/JavaScript.js
@@ -0,0 +1,8 @@
+﻿function inversion()
+{
+    let texto1 = document.getElementById("texto1").value;
+    let texto2 = document.getElementById("texto2").value;
+    let texto3 = document.getElementById("texto3");
+    texto3.style.display = "block";
+    texto3.value = texto2 + " " + texto1;
+}
